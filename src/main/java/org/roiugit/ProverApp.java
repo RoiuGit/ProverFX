bcd1ed4f92a1ca06c78e86468bcbee77a520c646
RoiuGit <puzikov.nikita@gmail.com>
2023-03-27 01:13:32 +0300

    added an icon

--- src/main/java/org/roiugit/ProverApp.java
+++ src/main/java/org/roiugit/ProverApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,6 +24,7 @@ public class ProverApp extends Application {
             primaryStage.setScene(new Scene(root));
             primaryStage.setMinWidth(800);
             primaryStage.setMinHeight(600);
+            primaryStage.getIcons().add(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("provericon.png"))));
             primaryStage.show();
         } catch (IOException e) {
             throw new RuntimeException(e);
